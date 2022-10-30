cac79fa000a0d93c997662aa3f984e5cf9814b65
MadPapa <happyekkov@gmail.com>
2022-10-30 18:36:33 +0100

    new folders hierarchy, new footer loook

--- php/about.php
+++ php/about.php
@@ -5,10 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="Author" content="Adrian Sidor">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="shortcut icon" href="orange_favicon.ico" type="image/x-icon">
-    <script src="js/kolorujtlo.js" type="text/javascript"></script>
+    <link rel="shortcut icon" href="../img/orange_favicon.ico" type="image/x-icon">
+    <script src="../js/kolorujtlo.js" type="text/javascript"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
     <title>about</title>
 </head>
@@ -17,11 +17,11 @@
         <header>
             <nav>
                 <ul class="nav-items">
-                    <li><a href="index.html">home</a></li>
-                    <li><a href="fun_fact.html">fun fact</a></li>
-                    <li><a href="species.html">species</a></li>
-                    <li><a href="events.html">events</a></li>
-                    <li><a href="store.html">store</a></li>
+                    <li><a href="../index.html">home</a></li>
+                    <li><a href="../html/fun_fact.html">fun fact</a></li>
+                    <li><a href="../html/species.html">species</a></li>
+                    <li><a href="../html/events.html">events</a></li>
+                    <li><a href="../html/store.html">store</a></li>
                     <li class="active"><a href="#">about</a></li>
                     <li><a href="mailto:162580@student.uwm.edu.pl?/Send mail">contact</a></li>
                 </ul>
@@ -83,8 +83,26 @@
                     });
                 </script>
             </div>
+            <div id="task-3">
+                <div>
+                    <?php include '../php/labor_162580_4.php';?>
+                </div>
+            </div>
         </section>
-        <footer>AS &copy 2022</footer>
+        <footer>
+            <div class="footer-text">
+                <p>AS &copy 2022</p>
+            </div>
+            <div class="footer-items">
+                <div><a href="../index.html">home</div>
+                <div><a href="../html/fun_fact.html">fun fuct</div>
+                <div><a href="../html/species.html">species</div>
+                <div><a href="../html/events.html">events</div>
+                <div><a href="../html/store.html">store</div>
+                <div><a href="../html/about.php">about</div>
+                <div><a href="../html/contact.html">contact</div>
+            </div>
+        </footer>
     </div>
 </body>
 </html>
---
+++ php/labor_162580_4.php
@@ -0,0 +1,7 @@
+<?php
+
+$nr_indeksu = '162580';
+$nrGrupy = '4';
+
+echo 'Adrian Sidor'.$nr_indeksu.'grupa'.$nrGrupy.'<br>';
+echo 'Zastosowanie metody include()'.'<br>';
